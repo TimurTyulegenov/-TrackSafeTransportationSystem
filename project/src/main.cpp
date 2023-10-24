1e5c5cc70b776210cff55927426264db42899202
TimurTyulegenov <88429725+TimurTyulegenov@users.noreply.github.com>
2023-10-24 14:15:21 +0500

    Update main.cpp

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,65 +1,34 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2006-2013 ARM Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include <events/mbed_events.h>
 #include "mbed.h"
 #include "ble/BLE.h"
 #include "ble/services/HealthThermometerService.h"
 #include "pretty_printer.h"
-#include <Hx711.h>
 
-DigitalOut led1(LED1, 1);
-Hx711 s1(P0_11,P0_12);
+AnalogIn   ain(P0_4);
+DigitalOut led1(LED2, 1);
+DigitalOut out1(P0_11, 1);
 
-UUID user_uuid("00001101-0000-1000-8000-00805F9B34FB");
-GattCharacteristic characteristic(
-        user_uuid,
-        /* valuePtr */ NULL,
-        /* len */ 1,
-        /* maxLen */ 20,
-        /* props */ GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE
-        );
-
-const static char DEVICE_NAME[] = "Trak";
+const static char DEVICE_NAME[] = "TensitionSensor"
 
 static events::EventQueue event_queue(/* event count */ 16 * EVENTS_EVENT_SIZE);
 
-class Track : ble::Gap::EventHandler {
+class ThermometerDemo : ble::Gap::EventHandler {
 public:
-    Track(BLE &ble, events::EventQueue &event_queue) :
+    ThermometerDemo(BLE &ble, events::EventQueue &event_queue) :
         _ble(ble),
         _event_queue(event_queue),
         _sensor_event_id(0),
-        _thermometer_uuid(GattService::UUID_HEALTH_THERMOMETER_SERVICE),
-        _user_uuid(user_uuid),
-        _current_temperature(39.6f),
-        _thermometer_service(NULL),
+        _tensitionsens_uuid(GattService::UUID_HEALTH_tensitionr_service),
+        _current_tensition(0),
+        _tensitionr_service(NULL),
         _adv_data_builder(_adv_buffer) { }
 
     void start() {
-
-        offset = (int)s1.read();
-        s1.set_offset(offset);
-        s1.set_scale(calibration_factor);
-
         _ble.gap().setEventHandler(this);
 
-        _ble.init(this, &Track::on_init_complete);
+        _ble.init(this, &ThermometerDemo::on_init_complete);
 
-        _event_queue.call_every(500, this, &Track::blink);
+        _event_queue.call_every(1000, this, &ThermometerDemo::blink);
 
         _event_queue.dispatch_forever();
     }
@@ -75,7 +44,7 @@ private:
         print_mac_address();
 
         /* Setup primary service. */
-        _thermometer_service = new HealthThermometerService(_ble, _current_temperature, HealthThermometerService::LOCATION_EAR);
+        _tensitionr_service = new HealthThermometerService(_ble, _current_tensition, HealthThermometerService::LOCATION_EAR);
 
         start_advertising();
     }
@@ -89,8 +58,7 @@ private:
         );
 
         _adv_data_builder.setFlags();
-        /*UUID *h[2]={&_thermometer_uuid,&_user_uuid};*/
-        _adv_data_builder.setLocalServiceList(mbed::make_Span(&_thermometer_uuid, 1));
+        _adv_data_builder.setLocalServiceList(mbed::make_Span(&_tensitionsens_uuid, 1));
         _adv_data_builder.setAppearance(ble::adv_data_appearance_t::THERMOMETER_EAR);
         _adv_data_builder.setName(DEVICE_NAME);
 
@@ -127,14 +95,15 @@ private:
     }
 
     void update_sensor_value() {
-        units_raw = s1.readRaw();
-        units = s1.read();
-        if (units < 0)
-        {
-            units = 0.00;
-        }
-        printf("Actual reading: %f grams. \n",units);
-        _thermometer_service->updateTemperature(units);
+       out1=true;
+	for(int i =0;i>=50;i++)
+	{
+	_current_tensition =(_current_tensition +ain.read()*100)/i;
+	ThisThread::sleep_for(25ms);
+	}
+         printf("%f \n",_current_tensition);
+        _tensitionr_service->updateTemperature((_current_tensition+_current_tensition1+_current_tensition2)/3);
+        out1=false;
     }
 
     void blink(void) {
@@ -151,7 +120,7 @@ private:
 
     virtual void onConnectionComplete(const ble::ConnectionCompleteEvent &event) {
         if (event.getStatus() == BLE_ERROR_NONE) {
-            _sensor_event_id = _event_queue.call_every(1000, this, &Track::update_sensor_value);
+            _sensor_event_id = _event_queue.call_every(1000, this, &ThermometerDemo::update_sensor_value);
         }
     }
 
@@ -161,34 +130,25 @@ private:
 
     int _sensor_event_id;
 
-    UUID _thermometer_uuid;
-    UUID _user_uuid;
+    UUID _tensitionsens_uuid;
 
-    float _current_temperature;
-    HealthThermometerService *_thermometer_service;
+    float _current_tensition;
+    HealthThermometerService *_tensitionr_service;
 
     uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
     ble::AdvertisingDataBuilder _adv_data_builder;
-
-    float   calibration_factor = -140.35f; // this calibration factor is adjusted according to my load cel
-    int32_t units_raw;
-    float   units;
-    float   ounces;
-    int     offset = 0;
-
 };
 
 /** Schedule processing of events from the BLE middleware in the event queue. */
 void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *context) {
     event_queue.call(Callback<void()>(&context->ble, &BLE::processEvents));
-}
-
+}*/
 int main()
 {
     BLE &ble = BLE::Instance();
     ble.onEventsToProcess(schedule_ble_events);
 
-    Track demo(ble, event_queue);
+    ThermometerDemo demo(ble, event_queue);
     demo.start();
 
     return 0;
